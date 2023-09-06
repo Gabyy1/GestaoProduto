1b0169078ddfdf3e0fc6e5143f59d8040d9ff5b0
Gabyy1 <100389136+Gabyy1@users.noreply.github.com>
2023-09-05 21:48:55 -0300

    Add files via upload

--- GestaoProduto.sql
+++ GestaoProduto.sql
@@ -5,16 +5,14 @@ Go
 
 Create  table  Produto (
 codigo	int		identity	not null primary key,
-nome	varchar(40)			not null,
-data	date				not null
+nome	varchar(40)				null,
+data	date					null
 )
 Go
 
 Insert Into Produto values ('alface', '20230912')
 Go
 Insert Into Produto values ('abacaxi', '20230912')
-Go
-Insert Into Produto values ( null, '20231010')
 Go
 
 Select * From Produto
